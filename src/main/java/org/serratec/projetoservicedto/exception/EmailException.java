e8229a23a485af53e90da429c81ec892ef62d55b
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2021-11-08 15:29:22 -0300

    Criando o usuarioRepository, Service e EmailException

---
+++ src/main/java/org/serratec/projetoservicedto/exception/EmailException.java
@@ -0,0 +1,10 @@
+package org.serratec.projetoservicedto.exception;
+
+
+public class EmailException extends RuntimeException{
+	
+	public EmailException(String message) {
+		super(message);
+	}
+
+}
